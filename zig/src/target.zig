e6052e2cb1ba2e5e2cde531f7bb190ac385715a9
Joel Billaud <joel.billaud@pm.me>
2026-05-12 15:14:54 +0900

    Zig patch: enable Xtensa target

--- zig/src/target.zig
+++ zig/src/target.zig
@@ -221,6 +221,7 @@ pub fn hasLlvmSupport(target: *const std.Target, ofmt: std.Target.ObjectFormat) 
         .x86,
         .x86_64,
         .xcore,
+        .xtensa,
         .nvptx,
         .nvptx64,
         .lanai,
@@ -231,7 +232,6 @@ pub fn hasLlvmSupport(target: *const std.Target, ofmt: std.Target.ObjectFormat) 
 
         // LLVM backend exists but can produce neither assembly nor object files.
         .csky,
-        .xtensa,
         => false,
 
         // No LLVM backend exists.
